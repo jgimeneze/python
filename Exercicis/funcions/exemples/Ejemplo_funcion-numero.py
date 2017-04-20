5825ad3386f00bdecf4e299dc78ef4031a53c095
jgimeneze <jgimeneze@gmail.com>
2017-04-20 12:17:50 +0200

    Create Ejemplo_funcion-numero.py

---
+++ Exercicis/funcions/exemples/Ejemplo_funcion-numero.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+def sumar(a,b):
+	resultado=0
+	
+	resultado=a+b
+	
+	return resultado
+	
+	
+#############################################################################	
+num1=0
+num2=0
+
+
+num1=input("Introduce número1: ")
+num2=input("Introduce número2: ")
+
+total=sumar(num1,num2)
+
+print "La suma de " , num1, " y " , num2, "es igual a ", total
+
