b2bea9c596d86b28289f376f28e8bb3e1d552df5
jgimeneze <jgimeneze@gmail.com>
2017-04-20 12:40:57 +0200

    Update Ejemplo_accion-parametros-modificables.py

--- Exercicis/funcions/exemples/Ejemplo_accion-parametros-modificables.py
+++ Exercicis/funcions/exemples/Ejemplo_accion-parametros-modificables.py
@@ -1,30 +1,46 @@
 # coding:utf-8
+
+# PARAMETROS
+# ---------------------------------------------------------------
+# nombre          : entrada          valor       no modificable
+# apellido1       : entrada/salida   referencia  modificable
+# apellido2       : entrada/salida   referencia  modificable
+# nombre_completo : salida           referencia  modificable
+# ---------------------------------------------------------------
 def mensaje_bienvenida(nombre, apellido1, apellido2):
 	print "*****************************"
 	print "          BIENVENIDO         "
 	print " ", nombre , "," , apellido1, "," , apellido2
 	print "*****************************"
 	
+	nombre_completo=nombre+","+apellido1+" "+apellido2
+	
 	nombre="CAMBIO"
 	apellido1="CAMBIAZO"
 	apellido2="CAMBIAZON"
 	
-	return apellido1,apellido2
+	
+	
+	return apellido1,apellido2,nombre_completo
 	
 #############################################################################	
 
 nombre_invitado="PEPE"
 apellido1_invitado="SANCHEZ"
 apellido2_invitado="LOPEZ"
+nombre_completo_invitado=""
 
-print nombre_invitado , apellido1_invitado, apellido2_invitado
+print nombre_invitado , apellido1_invitado, apellido2_invitado,nombre_completo_invitado
 
 
-# nombre_invitado    : parámetro no modificable
-# apellido1_invitado : parámetro modificable
-# apellido2_invitado : parámetro modificable
-apellido1_invitado,apellido2_invitado=mensaje_bienvenida(nombre_invitado,apellido1_invitado,apellido2_invitado)
+# ----------------------------------------------------------------------
+# nombre_invitado          : entrada          valor       no modificable
+# apellido1_invitado       : entrada/salida   referencia  modificable
+# apellido2_invitado       : entrada/salida   referencia  modificable
+# nombre_completo_invitado : salida           referencia  modificable
+# ----------------------------------------------------------------------
+apellido1_invitado,apellido2_invitado,nombre_completo_invitado=mensaje_bienvenida(nombre_invitado,apellido1_invitado,apellido2_invitado)
 
 
 # Sólo cambia el valor de los parámetro modificables
-print nombre_invitado , apellido1_invitado, apellido2_invitado
+print nombre_invitado , apellido1_invitado, apellido2_invitado,nombre_completo_invitado
