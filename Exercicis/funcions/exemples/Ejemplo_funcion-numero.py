29fd8adc9eedf536529ba16468777c92a74f1c5b
jgimeneze <jgimeneze@gmail.com>
2019-03-07 12:03:12 +0100

    Update Ejemplo_funcion-numero.py

--- Exercicis/funcions/exemples/Ejemplo_funcion-numero.py
+++ Exercicis/funcions/exemples/Ejemplo_funcion-numero.py
@@ -1,21 +1,20 @@
 # coding:utf-8
-def sumar(a,b):
-	resultado=0
-	
-	resultado=a+b
-	
-	return resultado
-	
-	
-#############################################################################	
-num1=0
-num2=0
+"""Sumamos operando1 (entero) y operando2 (entero)
+Devolvemos el resultado (entero)
+"""
+def sumar(operando1,operando2):
+    return operando1+operando2
 
 
-num1=input("Introduce número1: ")
-num2=input("Introduce número2: ")
+#############################################################################
+if __name__ == "__main__":
+    num1=0
+    num2=0
 
-total=sumar(num1,num2)
 
-print "La suma de " , num1, " y " , num2, "es igual a ", total
+    num1=int(input("Introduce número1: "))
+    num2=int(input("Introduce número2: "))
 
+    total=sumar(num1,num2)
+
+    print("La suma de " , num1, " y " , num2, "es igual a ",
